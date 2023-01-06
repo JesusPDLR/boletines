3f58b83f236b56a3526e27d6535fa4a34a660ec1
Jesus Pereyra de los Reyes <118146287+JesusPDLR@users.noreply.github.com>
2023-01-06 10:51:57 -0600

    first commit

---
+++ java/main.js
@@ -0,0 +1,30 @@
+/* let links = document.querySelectorAll("a");
+links.forEach(function(link) {
+console.log(link);
+});
+*/
+
+let links = document.querySelectorAll(".cerrar");
+
+links. forEach(function(link) {
+  link.addEventListener("click",function(ev){
+    ev.preventDefault();
+
+          let content = document.querySelector('.content');
+            content.classList.remove("animate__backInDown");
+            content.classList.remove("animate__animated");
+
+            content.classList.add("animate__backOutUp");
+            content.classList.add("animate__animated");
+
+            setTimeout(function(){
+              location.href ="/";
+            },600);
+    //return false;
+  });
+});
+
+/* let iconos = document.querySelectorAll("i")
+ iconos.forEach(function(icono){
+   icono.classList.remove("fa-star");
+   icono.classList.add("fa-star"); })*/
